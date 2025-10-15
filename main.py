8be30536e4e380f8e5e4ee5ace4a6e7800615d53
chetansri19-ai <chetansri19@gmail.com>
2025-10-15 16:44:14 +1100

    Lesson 1 Activity 1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+# printing a greeting message
 print('Hello, World!')
+
+# printing my name
+print('My name is Chetan.')
+
+
+# printing my hobbies
+print('My hobbies are reading books, playing chess, and coding.')
